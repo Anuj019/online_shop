ef2fa4ba1089e349a2a5f2475e7b4a75c82952f3
Anuj019 <anujjha0011@gmail.com>
2022-03-17 13:50:30 +0530

    componenet added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div >
-    <h1> My Online Shop </h1>
+   
     <HomePage/>
 
     </div>
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Directory from '../../componenets/directory/directory.component'
+
+
+import "./homepage.style.scss"
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+        
+            <Directory/>
+    </div>
+  )
+}
+
+export default HomePage;
